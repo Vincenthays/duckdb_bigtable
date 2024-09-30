0f86fc199db30a7e93bcd52dad82df3b6d0280d7
Vincent HAYS <vincenthays95@gmail.com>
2024-09-30 11:17:56 +0200

    clean code

--- src/bigtable2_extension.cpp
+++ src/bigtable2_extension.cpp
@@ -53,12 +53,20 @@ static unique_ptr<FunctionData> Bigtable2FunctionBind(ClientContext &context, Ta
 
 void Bigtable2Function(ClientContext &context, TableFunctionInput &data, DataChunk &output) {
     auto &state = (Bigtable2FunctionData &)*data.bind_data;
+    idx_t cardinality = 0; 
 
     for (StatusOr<cbt::Row>& row : state.table->ReadRows(
         cbt::RowSet("30000000001/202231/38590", "30000000001/202231/38593"),
         cbt::Filter::PassAllFilter()
     )) {
         if (!row) throw std::move(row).status();
+
+        vector<Value> promo_id;
+        vector<Value> promo_text;
+        vector<Value> shelf;
+        vector<Value> position;
+        vector<Value> is_paid;
+        
         for (auto& cell : row.value().cells()) {
             std::cout 
                 << cell.family_name()
@@ -67,7 +75,42 @@ void Bigtable2Function(ClientContext &context, TableFunctionInput &data, DataChu
                 << "="
                 << cell.value()
                 << std::endl;
+
+            switch (cell.family_name().at(0)) {
+            case 'p':
+                switch (cell.column_qualifier().at(0)) {
+                case 'p':
+                    output.SetValue(2, state.row_idx, Value(std::stof(cell.value())));
+                    break;
+                case 'b':
+                    output.SetValue(3, state.row_idx, Value(std::stof(cell.value())));
+                    break;
+                case 'u':
+                    output.SetValue(4, state.row_idx, Value(std::stof(cell.value())));
+                    break;
+                }
+                break;
+            case 'd':
+                promo_id.emplace_back(Value(std::stoi(cell.column_qualifier())));
+                promo_text.emplace_back(Value(cell.value()));
+                break;
+            case 's' | 'S':
+                shelf.emplace_back(cell.column_qualifier());
+                position.emplace_back(Value(std::stoi(cell.value())));
+                is_paid.emplace_back(Value(cell.column_qualifier().at(0) == 'S'));
+                break;
+            default:
+                break;
+            }
         }
+        output.SetValue(5, state.row_idx, Value::LIST(LogicalType::UINTEGER, std::move(promo_id)));
+        output.SetValue(6, state.row_idx, Value::LIST(LogicalType::VARCHAR, std::move(promo_text)));
+        output.SetValue(7, state.row_idx, Value::LIST(LogicalType::VARCHAR, std::move(shelf)));
+        output.SetValue(8, state.row_idx, Value::LIST(LogicalType::UINTEGER, std::move(position)));
+        output.SetValue(9, state.row_idx, Value::LIST(LogicalType::BOOLEAN, std::move(is_paid)));
+
+        cardinality++;
+        state.row_idx++;
     }
 
     if (state.row_idx >= 100) {
@@ -75,12 +118,11 @@ void Bigtable2Function(ClientContext &context, TableFunctionInput &data, DataChu
         return;
     }
 
-    output.SetValue(0, state.row_idx++, Value::INTEGER(2));
-    output.SetCardinality(1);
+    output.SetCardinality(cardinality);
 }
 
 void Bigtable2Extension::Load(DuckDB &db) {
-    TableFunction bigtable_function("bigtable2", {LogicalType::LIST(LogicalType::UBIGINT)}, Bigtable2Function, Bigtable2FunctionBind);
+    TableFunction bigtable_function("bigtable2", {LogicalType::LIST(LogicalType::INTEGER)}, Bigtable2Function, Bigtable2FunctionBind);
     ExtensionUtil::RegisterFunction(*db.instance, bigtable_function);
 }
 
