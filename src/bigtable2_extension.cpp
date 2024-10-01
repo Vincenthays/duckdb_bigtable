4731b241be0468e05803aa85dbeb8e1a73aa4a4a
Vincent HAYS <vincenthays95@gmail.com>
2024-10-01 18:00:49 +0200

    clean code

--- src/bigtable2_extension.cpp
+++ src/bigtable2_extension.cpp
@@ -133,7 +133,7 @@ void Bigtable2Function(ClientContext &context, TableFunctionInput &data, DataChu
 
         for (int i = 0; i < 7; i++) {
             auto date = arr_date[i];
-            if (!date.IsNull()) {
+            if (date > Value::DATE(1970, 1, 1)) {
                 output.SetValue(0, state.row_idx, Value::UBIGINT(pe_id));
                 output.SetValue(1, state.row_idx, date);
                 output.SetValue(2, state.row_idx, Value::UINTEGER(shop_id));
