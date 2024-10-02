c4e8bff1d6a4930d6b87a1fd37f3be4be5adcc49
Vincent HAYS <vincenthays95@gmail.com>
2024-10-02 16:49:25 +0200

    clean code

--- src/bigtable2_extension.cpp
+++ src/bigtable2_extension.cpp
@@ -87,7 +87,6 @@ void Bigtable2Function(ClientContext &context, TableFunctionInput &data, DataChu
         reverse(prefix_id.begin(), prefix_id.end());
 
         uint64_t pe_id = std::stoul(prefix_id);
-        string_t date = row_key.substr(index_1 + 1, index_2 - index_1 - 1);
         uint32_t shop_id = std::stoul(row_key.substr(index_2 + 1));
 
         bool arr_mask[7];
