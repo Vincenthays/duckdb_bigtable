180e03980197a9ce1e5d9adf5b51105f95dd5709
Vincent HAYS <vincenthays95@gmail.com>
2024-10-04 14:08:26 +0200

    clean code

--- src/bigtable2_extension.cpp
+++ src/bigtable2_extension.cpp
@@ -152,9 +152,12 @@ void Bigtable2Function(ClientContext &context, TableFunctionInput &data, DataChu
       output.SetValue(5, state.row_idx, arr_unit_price[i]);
       output.SetValue(6, state.row_idx, arr_promo_id[i]);
       output.SetValue(7, state.row_idx, arr_promo_text[i]);
-      if (!arr_shelf[i].empty()) output.SetValue(8, state.row_idx, Value::LIST(arr_shelf[i]));
-      if (!arr_position[i].empty()) output.SetValue(9, state.row_idx, Value::LIST(arr_position[i]));
-      if (!arr_is_paid[i].empty()) output.SetValue(10, state.row_idx, Value::LIST(arr_is_paid[i]));
+
+      if (!arr_shelf[i].empty()) {
+        output.SetValue(8, state.row_idx, Value::LIST(arr_shelf[i]));
+        output.SetValue(9, state.row_idx, Value::LIST(arr_position[i]));
+        output.SetValue(10, state.row_idx, Value::LIST(arr_is_paid[i]));
+      }
 
       cardinality++;
       state.row_idx++;
