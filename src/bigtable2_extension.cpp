1539935a55f1b7d6870ccc195a6a2a85e6e45363
Vincent HAYS <vincenthays95@gmail.com>
2024-10-04 17:36:44 +0200

    clean code

--- src/bigtable2_extension.cpp
+++ src/bigtable2_extension.cpp
@@ -36,16 +36,16 @@ static unique_ptr<FunctionData> Bigtable2FunctionBind(ClientContext &context, Ta
   return_types.emplace_back(LogicalType::FLOAT);
   names.emplace_back("unit_price");
   return_types.emplace_back(LogicalType::FLOAT);
-  names.emplace_back("promo_id");
-  return_types.emplace_back(LogicalType::UINTEGER);
-  names.emplace_back("promo_text");
-  return_types.emplace_back(LogicalType::VARCHAR);
-  names.emplace_back("shelf");
-  return_types.emplace_back(LogicalType::LIST(LogicalType::VARCHAR));
-  names.emplace_back("position");
-  return_types.emplace_back(LogicalType::LIST(LogicalType::UINTEGER));
-  names.emplace_back("is_paid");
-  return_types.emplace_back(LogicalType::LIST(LogicalType::BOOLEAN));
+  // names.emplace_back("promo_id");
+  // return_types.emplace_back(LogicalType::UINTEGER);
+  // names.emplace_back("promo_text");
+  // return_types.emplace_back(LogicalType::VARCHAR);
+  // names.emplace_back("shelf");
+  // return_types.emplace_back(LogicalType::LIST(LogicalType::VARCHAR));
+  // names.emplace_back("position");
+  // return_types.emplace_back(LogicalType::LIST(LogicalType::UINTEGER));
+  // names.emplace_back("is_paid");
+  // return_types.emplace_back(LogicalType::LIST(LogicalType::BOOLEAN));
 
   auto bind_data = make_uniq<Bigtable2FunctionData>();
 
@@ -96,11 +96,11 @@ void Bigtable2Function(ClientContext &context, TableFunctionInput &data, DataChu
     Value arr_price[7];
     Value arr_base_price[7];
     Value arr_unit_price[7];
-    Value arr_promo_id[7];
-    Value arr_promo_text[7];
-    vector<Value> arr_shelf[7];
-    vector<Value> arr_position[7];
-    vector<Value> arr_is_paid[7];
+    // Value arr_promo_id[7];
+    // Value arr_promo_text[7];
+    // vector<Value> arr_shelf[7];
+    // vector<Value> arr_position[7];
+    // vector<Value> arr_is_paid[7];
 
     for (int i = 0; i < 7; i++) {
       arr_mask[i] = false;
@@ -129,13 +129,13 @@ void Bigtable2Function(ClientContext &context, TableFunctionInput &data, DataChu
         }
         break;
       case 'd':
-        arr_promo_id[weekday] = std::stoi(cell.column_qualifier());
-        arr_promo_text[weekday] = cell.value();
+        // arr_promo_id[weekday] = std::stoi(cell.column_qualifier());
+        // arr_promo_text[weekday] = cell.value();
         break;
       case 's': case 'S':
-        arr_shelf[weekday].emplace_back(cell.column_qualifier());
-        arr_position[weekday].emplace_back(std::stoi(cell.value()));
-        arr_is_paid[weekday].emplace_back(cell.family_name().at(0) == 'S');
+        // arr_shelf[weekday].emplace_back(cell.column_qualifier());
+        // arr_position[weekday].emplace_back(std::stoi(cell.value()));
+        // arr_is_paid[weekday].emplace_back(cell.family_name().at(0) == 'S');
         break;
       }
     }
@@ -149,14 +149,14 @@ void Bigtable2Function(ClientContext &context, TableFunctionInput &data, DataChu
       output.SetValue(3, state.row_idx, arr_price[i]);
       output.SetValue(4, state.row_idx, arr_base_price[i]);
       output.SetValue(5, state.row_idx, arr_unit_price[i]);
-      output.SetValue(6, state.row_idx, arr_promo_id[i]);
-      output.SetValue(7, state.row_idx, arr_promo_text[i]);
+      // output.SetValue(6, state.row_idx, arr_promo_id[i]);
+      // output.SetValue(7, state.row_idx, arr_promo_text[i]);
 
-      if (!arr_shelf[i].empty()) {
-        output.SetValue(8, state.row_idx, Value::LIST(arr_shelf[i]));
-        output.SetValue(9, state.row_idx, Value::LIST(arr_position[i]));
-        output.SetValue(10, state.row_idx, Value::LIST(arr_is_paid[i]));
-      }
+      // if (!arr_shelf[i].empty()) {
+      //   output.SetValue(8, state.row_idx, Value::LIST(arr_shelf[i]));
+      //   output.SetValue(9, state.row_idx, Value::LIST(arr_position[i]));
+      //   output.SetValue(10, state.row_idx, Value::LIST(arr_is_paid[i]));
+      // }
 
       cardinality++;
       state.row_idx++;
@@ -171,7 +171,9 @@ void Bigtable2Extension::Load(DuckDB &db) {
   ExtensionUtil::RegisterFunction(*db.instance, bigtable_function);
 }
 
-std::string Bigtable2Extension::Name() { return "bigtable2"; }
+std::string Bigtable2Extension::Name() { 
+  return "bigtable2"; 
+}
 
 std::string Bigtable2Extension::Version() const {
 #ifdef EXT_VERSION_BIGTABLE2
