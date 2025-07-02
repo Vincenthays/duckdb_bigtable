cedba30638afa05d947126018e6f7c31f2bf3c6f
Vincent <vincent.hays@nielseniq.com>
2025-07-02 10:27:16 +0200

    clean up

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,10 +1,11 @@
 #include "utils.hpp"
+#include "fast_float/fast_float.h"
 
-#include <string>
 #include <cstdint>
 #include <optional>
 #include <charconv>
-#include <stdexcept>
+#include <string_view>
+
 
 std::optional<uint8_t> ParseUint8(std::string_view s) {
 	uint8_t result;
@@ -34,12 +35,10 @@ std::optional<uint64_t> ParseUint64(std::string_view s) {
 }
 
 std::optional<float> ParseFloat(std::string_view s) {
-	try {
-		// std::stof requires a null-terminated string, so we create a temporary std::string.
-		return std::stof(std::string(s));
-	} catch (const std::invalid_argument &) {
-		return std::nullopt;
-	} catch (const std::out_of_range &) {
-		return std::nullopt;
+	float result;
+	auto [ptr, ec] = duckdb_fast_float::from_chars(s.data(), s.data() + s.size(), result);
+	if (ec == std::errc()) {
+		return result;
 	}
+	return std::nullopt;
 }
